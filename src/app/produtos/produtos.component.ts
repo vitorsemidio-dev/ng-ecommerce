947ea0de7a30b34bbf38441925634c40fed6bb85
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-05-19 21:36:52 -0300

    feat: mock produtos

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,15 +1,18 @@
+import { ProdutoModel } from './produto.model';
 import { Component, OnInit } from '@angular/core';
+import { produtosMock } from './produtos.mock';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
-  styleUrls: ['./produtos.component.scss']
+  styleUrls: ['./produtos.component.scss'],
 })
 export class ProdutosComponent implements OnInit {
+  produtos: ProdutoModel[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.produtos = produtosMock;
   }
-
 }
---
+++ src/app/produtos/promocao.model.ts
@@ -0,0 +1,10 @@
+export interface PromocaoModel {
+  id: string;
+  criadoEm: Date | string;
+  atualizadoEm: Date | string;
+  deletadoEm: Date | string | null;
+  leveX: number | null;
+  pagueY: number | null;
+  compreX: number | null;
+  porY: number | null;
+}
