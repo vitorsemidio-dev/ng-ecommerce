3e2fe83d8a8e42812dac1c7217338ca9a4da1388
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-05-19 22:03:50 -0300

    feat: initial template from page produtos

--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { ProdutosRoutingModule } from './produtos-routing.module';
 import { ProdutosComponent } from './produtos.component';
-
-
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    ProdutosComponent
-  ],
+  declarations: [ProdutosComponent],
   imports: [
     CommonModule,
-    ProdutosRoutingModule
-  ]
+    ProdutosRoutingModule,
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule,
+  ],
 })
-export class ProdutosModule { }
+export class ProdutosModule {}
