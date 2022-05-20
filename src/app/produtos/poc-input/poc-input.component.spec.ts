61eb5a491df3e18f6a26fb6e58a05c340ce8364b
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-05-19 22:52:57 -0300

    temp: poc input

---
+++ src/app/produtos/poc-input/poc-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PocInputComponent } from './poc-input.component';
+
+describe('PocInputComponent', () => {
+  let component: PocInputComponent;
+  let fixture: ComponentFixture<PocInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PocInputComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PocInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -5,10 +5,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ProdutosRoutingModule } from './produtos-routing.module';
 import { ProdutosComponent } from './produtos.component';
+import { PocInputComponent } from './poc-input/poc-input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [ProdutosComponent],
+  declarations: [ProdutosComponent, PocInputComponent],
   imports: [
     CommonModule,
     ProdutosRoutingModule,
@@ -17,6 +20,9 @@ import { ProdutosComponent } from './produtos.component';
     MatChipsModule,
     MatDialogModule,
     MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
 })
 export class ProdutosModule {}
