ae23d02068f7f89713a658b745d8e80424e3e528
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-05-20 02:08:15 -0300

    temp: initial template carrinho with table

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoModel } from '../produtos/produto.model';
+import { produtosMock } from '../produtos/produtos.mock';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
-  styleUrls: ['./carrinho.component.scss']
+  styleUrls: ['./carrinho.component.scss'],
 })
 export class CarrinhoComponent implements OnInit {
+  carrinho: ProdutoModel[] = produtosMock;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
